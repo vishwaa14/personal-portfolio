f9159ad35ce998af6baf26223eb894cfc631d0ef
Vishwaa <vishwaa.vishwaa2@gmail.com>
2025-06-03 01:24:44 +0530

    repo name fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/vishwaa-portfolio/"
-}));
+  base: "/personal-portfolio/",
+});
